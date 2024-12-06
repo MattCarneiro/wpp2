9f8fceb28e93574c4a1ab8b73053eb76b144acd3
Elementttt <121469830+MattCarneiro@users.noreply.github.com>
2024-12-05 21:37:28 -0300

    Update rabbitmq.ts

--- src/lib/rabbitmq.ts
+++ src/lib/rabbitmq.ts
@@ -1,17 +1,28 @@
 import amqp from 'amqplib';
 
 export async function setupRabbitMQ() {
-  const connection = await amqp.connect(process.env.RABBITMQ_URL!);
-  const channel = await connection.createChannel();
-  
-  await channel.prefetch(parseInt(process.env.RABBITMQ_PREFETCH || '10'));
-  
-  await channel.assertQueue('whatsapp_messages', { durable: true });
-  
-  channel.consume('whatsapp_messages', async (msg) => {
-    if (msg) {
-      console.log('Received message:', msg.content.toString());
-      channel.ack(msg);
-    }
-  });
+  try {
+    const connection = await amqp.connect(process.env.RABBITMQ_URL!);
+    const channel = await connection.createChannel();
+    
+    await channel.prefetch(parseInt(process.env.RABBITMQ_PREFETCH || '10'));
+    
+    // Declara a fila como do tipo 'quorum'
+    await channel.assertQueue('whatsapp_messages', { 
+      durable: true, 
+      arguments: { 'x-queue-type': 'quorum' } 
+    });
+    
+    channel.consume('whatsapp_messages', async (msg) => {
+      if (msg) {
+        console.log('Received message:', msg.content.toString());
+        channel.ack(msg);
+      }
+    });
+
+    console.log('RabbitMQ setup completed.');
+  } catch (error) {
+    console.error('Error setting up RabbitMQ:', error);
+    process.exit(1); // Encerra a aplicação em caso de erro crítico
+  }
 }
